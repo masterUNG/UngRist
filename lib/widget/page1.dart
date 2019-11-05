84841bf612f2595c244724e61f305b1d64029799
masterUNG <phrombutr@gmail.com>
2019-11-05 15:45:46 +0700

    ListView OK

--- lib/widget/page1.dart
+++ lib/widget/page1.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:ungrist/models/pageholder_model.dart';
 
 class Page1 extends StatefulWidget {
   @override
@@ -9,31 +10,64 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
-
   // Explicit
+  List<String> tests = ['AA', 'BB', 'CC'];
+  List<PageHolderModel> pageHolderModels = [];
 
   // Method
   @override
-  void initState() { 
+  void initState() {
     super.initState();
     readAllData();
     // Future<String> test = testThread();
   }
 
-  Future<String> testThread()async{
+  Future<String> testThread() async {
     return 'Answer';
   }
 
-  Future<void> readAllData()async{
+  Future<void> readAllData() async {
     String urlAPI = 'https://jsonplaceholder.typicode.com/posts';
     Response response = await get(urlAPI);
     var result = json.decode(response.body);
     print('result = $result');
+
+    for (var map in result) {
+      PageHolderModel holderModel = PageHolderModel.fromJson(map);
+      String title = holderModel.title;
+      print('title = $title');
+
+      setState(() {
+        pageHolderModels.add(holderModel);
+      });
+    }
   }
 
+  Widget showTitle(int index) {
+    return Text(pageHolderModels[index].title);
+  }
+
+  Widget showListView(int index) {
+    return Container(padding: EdgeInsets.all(10.0),
+      decoration: index % 2 == 0
+          ? BoxDecoration(color: Colors.blue)
+          : BoxDecoration(color: Colors.blue[100]),
+      child: Column(
+        children: <Widget>[
+          showTitle(index),
+          // Divider(),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Page 1'),);
+    return ListView.builder(
+      itemCount: pageHolderModels.length,
+      itemBuilder: (BuildContext context, int index) {
+        return showListView(index);
+      },
+    );
   }
 }
