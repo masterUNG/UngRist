19a620138c30bc0c3548abde5d3bd36ca3c9a66c
masterUNG <phrombutr@gmail.com>
2019-11-05 15:03:44 +0700

    update

--- lib/widget/page1.dart
+++ lib/widget/page1.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Page1 extends StatefulWidget {
   @override
@@ -6,6 +9,29 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+
+  // Explicit
+
+  // Method
+  @override
+  void initState() { 
+    super.initState();
+    readAllData();
+    // Future<String> test = testThread();
+  }
+
+  Future<String> testThread()async{
+    return 'Answer';
+  }
+
+  Future<void> readAllData()async{
+    String urlAPI = 'https://jsonplaceholder.typicode.com/posts';
+    Response response = await get(urlAPI);
+    var result = json.decode(response.body);
+    print('result = $result');
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Center(child: Text('Page 1'),);
